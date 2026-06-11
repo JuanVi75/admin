1c6d7eff04fc2a803bcb6b85f1d37e8e635fffb4
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-11 09:20:16 -0500

    Update departamentos.routes.js

--- routes/departamentos.routes.js
+++ routes/departamentos.routes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
 
+const controller = require("../controllers/departamentos.controller");
+
+// GET - listar departamentos
+router.get("/", controller.listar);
+
+module.exports = router;
