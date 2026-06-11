1cdc5fd182b7531a9af366e540772ecff2100ebd
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-11 11:16:46 -0500

    Update departamentos.controller.js

--- controllers/departamentos.controller.js
+++ controllers/departamentos.controller.js
@@ -1,5 +1,8 @@
 const Departamentos = require("../models/departamentos.model");
 
+/* =========================
+   LISTAR
+========================= */
 function listar(req, res) {
     Departamentos.listarDepartamentos((err, results) => {
         if (err) {
@@ -11,6 +14,61 @@ function listar(req, res) {
     });
 }
 
+/* =========================
+   CREAR
+========================= */
+function crear(req, res) {
+
+    const { id, depto } = req.body;
+
+    Departamentos.crearDepartamento(id, depto, (err, result) => {
+        if (err) {
+            console.log("Error en controller crear:", err);
+            return res.status(500).json({ error: "Error al crear departamento" });
+        }
+
+        res.json({ ok: true });
+    });
+}
+
+/* =========================
+   MODIFICAR
+========================= */
+function modificar(req, res) {
+
+    const id = req.params.id;
+    const { depto } = req.body;
+
+    Departamentos.modificarDepartamento(id, depto, (err, result) => {
+        if (err) {
+            console.log("Error en controller modificar:", err);
+            return res.status(500).json({ error: "Error al modificar departamento" });
+        }
+
+        res.json({ ok: true });
+    });
+}
+
+/* =========================
+   BORRAR
+========================= */
+function borrar(req, res) {
+
+    const id = req.params.id;
+
+    Departamentos.borrarDepartamento(id, (err, result) => {
+        if (err) {
+            console.log("Error en controller borrar:", err);
+            return res.status(500).json({ error: "Error al borrar departamento" });
+        }
+
+        res.json({ ok: true });
+    });
+}
+
 module.exports = {
-    listar
+    listar,
+    crear,
+    modificar,
+    borrar
 };
