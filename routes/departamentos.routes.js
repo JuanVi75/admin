fb66684509a0a2cd68da89d5444ce8dd8662bd92
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-11 09:32:49 -0500

    Update departamentos.routes.js

--- routes/departamentos.routes.js
+++ routes/departamentos.routes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const db = require("../config/db");
+
+// GET /departamentos (DATA REAL)
 router.get("/", (req, res) => {
-    res.json([
-        { id: 1, depto: "Antioquia (modular OK)" },
-        { id: 2, depto: "Cundinamarca (modular OK)" }
-    ]);
+
+    const sql = "SELECT id, depto FROM departamentos";
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.log("Error MySQL departamentos:", err);
+            return res.status(500).json({ error: "Error en base de datos" });
+        }
+
+        res.json(results);
+    });
 });
 
 module.exports = router;
