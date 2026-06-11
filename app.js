eb4e6253a9f15b7230cd6c441e843cc14d7b1144
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-11 09:30:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-/* TEST DIRECTO SIN ROUTES */
-app.get("/departamentos", (req, res) => {
-  res.json([
-    { id: 1, depto: "TEST ANTIOQUIA" },
-    { id: 2, depto: "TEST CUNDINAMARCA" }
-  ]);
-});
+/* ROUTES */
+const departamentosRoutes = require("./routes/departamentos.routes");
+app.use("/departamentos", departamentosRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
