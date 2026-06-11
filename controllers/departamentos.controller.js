58ae47fd5a4869b66527c85ed9617c24d935dc0a
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-11 09:14:14 -0500

    Update departamentos.controller.js

--- controllers/departamentos.controller.js
+++ controllers/departamentos.controller.js
@@ -1,1 +1,16 @@
+const Departamentos = require("../models/departamentos.model");
 
+function listar(req, res) {
+    Departamentos.listarDepartamentos((err, results) => {
+        if (err) {
+            console.log("Error en controller departamentos:", err);
+            return res.status(500).json({ error: "Error en base de datos" });
+        }
+
+        res.json(results);
+    });
+}
+
+module.exports = {
+    listar
+};
