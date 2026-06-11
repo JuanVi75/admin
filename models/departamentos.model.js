7752f09247aa82979e72a0c3e8b4d5ea07573ec0
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-11 11:17:59 -0500

    Update departamentos.model.js

--- models/departamentos.model.js
+++ models/departamentos.model.js
@@ -1,12 +1,51 @@
 const db = require("../config/db");
 
+/* =========================
+   LISTAR
+========================= */
 function listarDepartamentos(callback) {
-    const sql = "SELECT * FROM departamentos";
+    const sql = "SELECT * FROM departamentos ORDER BY id ASC";
     db.query(sql, (err, results) => {
         callback(err, results);
     });
 }
 
+/* =========================
+   CREAR
+========================= */
+function crearDepartamento(id, depto, callback) {
+    const sql = "INSERT INTO departamentos (id, depto) VALUES (?, ?)";
+
+    db.query(sql, [id, depto], (err, result) => {
+        callback(err, result);
+    });
+}
+
+/* =========================
+   MODIFICAR
+========================= */
+function modificarDepartamento(id, depto, callback) {
+    const sql = "UPDATE departamentos SET depto = ? WHERE id = ?";
+
+    db.query(sql, [depto, id], (err, result) => {
+        callback(err, result);
+    });
+}
+
+/* =========================
+   BORRAR
+========================= */
+function borrarDepartamento(id, callback) {
+    const sql = "DELETE FROM departamentos WHERE id = ?";
+
+    db.query(sql, [id], (err, result) => {
+        callback(err, result);
+    });
+}
+
 module.exports = {
-    listarDepartamentos
+    listarDepartamentos,
+    crearDepartamento,
+    modificarDepartamento,
+    borrarDepartamento
 };
