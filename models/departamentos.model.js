5c143e079f7912ba735eeccab3fa287b49f9ee15
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-11 09:14:37 -0500

    Update departamentos.model.js

--- models/departamentos.model.js
+++ models/departamentos.model.js
@@ -1,1 +1,12 @@
+const db = require("../config/db");
 
+function listarDepartamentos(callback) {
+    const sql = "SELECT * FROM departamentos";
+    db.query(sql, (err, results) => {
+        callback(err, results);
+    });
+}
+
+module.exports = {
+    listarDepartamentos
+};
