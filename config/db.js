2ac45aa3dcdfb379b6d265c4abd22150b4b0a26c
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-11 08:59:36 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,24 @@
+const mysql = require("mysql2");
 
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Verificación de conexión
+db.getConnection((err, connection) => {
+    if (err) {
+        console.log("❌ Error conexión MySQL:", err.message);
+    } else {
+        console.log("✅ MySQL conectado correctamente");
+        connection.release();
+    }
+});
+
+module.exports = db;
