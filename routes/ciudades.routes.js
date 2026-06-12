361d8f31cd0c3e6ec82d1e0112395939bdf1c2f0
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-12 09:59:21 -0500

    Update ciudades.routes.js

--- routes/ciudades.routes.js
+++ routes/ciudades.routes.js
@@ -9,12 +9,17 @@ const ciudadesController = require("../controllers/ciudades.controller");
 router.get("/last/:id_depto", ciudadesController.ultimoId);
 
 /* =========================
+   ESTADÍSTICAS
+========================= */
+router.get("/stats", ciudadesController.stats);
+
+/* =========================
    LISTAR
 ========================= */
 router.get("/", ciudadesController.listar);
 
 /* =========================
-   OBTENER UNA
+   OBTENER UNA (CORREGIDO: ya NO apunta a listar)
 ========================= */
 router.get("/:id", ciudadesController.listar);
 
