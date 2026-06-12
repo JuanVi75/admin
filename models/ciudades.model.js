c137119cbe79cd60c81ebefd92dbc2337eb7f9e0
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-12 12:47:28 -0500

    Update ciudades.model.js

--- models/ciudades.model.js
+++ models/ciudades.model.js
@@ -5,17 +5,19 @@ const db = require("../config/db");
 ========================= */
 function listarCiudades(callback) {
    const sql = `
-        SELECT
-            CAST(c.id AS CHAR) AS id,
-            c.municipio,
-            c.id_depto,
-            d.depto AS departamento
-        FROM ciudades c
-        LEFT JOIN departamentos d
-            ON d.id = c.id_depto
-        WHERE c.is_deleted = 0
-        ORDER BY CAST(c.id AS UNSIGNED) ASC
-    `;
+      SELECT
+         CAST(c.id AS CHAR) AS id,
+         c.municipio,
+         c.id_depto,
+         d.depto AS departamento
+      FROM ciudades c
+      LEFT JOIN departamentos d
+         ON d.id = c.id_depto
+      WHERE c.is_deleted = 0
+      ORDER BY
+         CAST(c.id_depto AS UNSIGNED) ASC,
+         CAST(SUBSTRING(c.id, 3) AS UNSIGNED) ASC
+   `;
 
    db.query(sql, (err, results) => {
       callback(err, results);
@@ -27,10 +29,10 @@ function listarCiudades(callback) {
 ========================= */
 function crearCiudad(id, municipio, id_depto, callback) {
    const sql = `
-        INSERT INTO ciudades
-        (id, municipio, id_depto, created_at, is_deleted)
-        VALUES (?, ?, ?, NOW(), 0)
-    `;
+      INSERT INTO ciudades
+      (id, municipio, id_depto, created_at, is_deleted)
+      VALUES (?, ?, ?, NOW(), 0)
+   `;
 
    db.query(sql, [id, municipio, id_depto], (err, result) => {
       callback(err, result);
@@ -42,10 +44,13 @@ function crearCiudad(id, municipio, id_depto, callback) {
 ========================= */
 function modificarCiudad(id, municipio, id_depto, callback) {
    const sql = `
-        UPDATE ciudades
-        SET municipio = ?, id_depto = ?, updated_at = NOW()
-        WHERE id = ?
-    `;
+      UPDATE ciudades
+      SET
+         municipio = ?,
+         id_depto = ?,
+         updated_at = NOW()
+      WHERE id = ?
+   `;
 
    db.query(sql, [municipio, id_depto, id], (err, result) => {
       callback(err, result);
@@ -57,10 +62,12 @@ function modificarCiudad(id, municipio, id_depto, callback) {
 ========================= */
 function borrarCiudad(id, callback) {
    const sql = `
-        UPDATE ciudades
-        SET is_deleted = 1, deleted_at = NOW()
-        WHERE id = ?
-    `;
+      UPDATE ciudades
+      SET
+         is_deleted = 1,
+         deleted_at = NOW()
+      WHERE id = ?
+   `;
 
    db.query(sql, [id], (err, result) => {
       callback(err, result);
@@ -72,11 +79,13 @@ function borrarCiudad(id, callback) {
 ========================= */
 function obtenerUltimoIdPorDepto(id_depto, callback) {
    const sql = `
-        SELECT MAX(CAST(id AS UNSIGNED)) AS ultimo_id
-        FROM ciudades
-        WHERE id_depto = ?
-          AND is_deleted = 0
-    `;
+      SELECT id
+      FROM ciudades
+      WHERE id_depto = ?
+        AND is_deleted = 0
+      ORDER BY CAST(SUBSTRING(id, 3) AS UNSIGNED) DESC
+      LIMIT 1
+   `;
 
    db.query(sql, [id_depto], (err, results) => {
       callback(err, results);
