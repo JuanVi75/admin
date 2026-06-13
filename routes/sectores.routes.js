c38516813053eb9f4e808a5a1058e5e1b0476d87
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-13 11:35:30 -0500

    Update sectores.routes.js

--- routes/sectores.routes.js
+++ routes/sectores.routes.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const SectoresController = require('../controllers/sectores.controller');
 
-router.get('/', SectoresController.getAll);
-router.post('/', SectoresController.create);
-router.put('/:id', SectoresController.update);
-router.delete('/:id', SectoresController.delete);
+/* LISTAR */
+router.get('/', SectoresController.listar);
+/* CREAR */
+router.post('/', SectoresController.crear);
+/* MODIFICAR */
+router.put('/:id', SectoresController.modificar);
+/* ELIMINAR */
+router.delete('/:id', SectoresController.borrar);
+/* STATS */
 router.get('/stats', SectoresController.stats);
 
 module.exports = router;
